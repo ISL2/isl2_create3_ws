a643b5a3b31c3db2bf5637bff943975edf5147b0
mrKornWij <88375996+mrKornWij@users.noreply.github.com>
2023-07-04 16:33:23 +0700

    add launch files

---
+++ src/bringup/launch/bringup_launch.py
@@ -0,0 +1,14 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+    joy_launch = IncludeLaunchDescription(
+      PythonLaunchDescriptionSource([os.path.join(
+         get_package_share_directory('launch_tutorial'), 'launch'),
+         '/turtlesim_world_1.launch.py'])
+      )
+    return LaunchDescription([
+        joy_launch
+    ])
---
+++ src/bringup/launch/joy_launch.py
@@ -0,0 +1,33 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, TextSubstitution
+
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    config_filepath = LaunchConfiguration('config_filepath')
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'config_filepath',
+            default_value=[
+                TextSubstitution(text=os.path.join(get_package_share_directory('teleop_twist_joy'), 'config', '')),
+                joy_config, TextSubstitution(text='.config.yaml')
+            ]
+        ),
+        Node(
+            package='joy',
+            namespace='joy',
+            executable='joy_node',
+            name='joy_input'
+        ),
+        Node(
+            package='teleop_twist_joy',
+            namespace='joy',
+            executable='teleop_node',
+            name='joy_twist'
+            parameters=[config_filepath]
+        ),
+    ])
