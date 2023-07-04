60458c5c4c042e21697db5e384cdc77917402b80
ISL2 <isl2.chula@gmail.com>
2023-07-04 10:34:10 +0000

    minor fix

--- src/bringup/launch/bringup.launch.py
+++ src/bringup/launch/bringup.launch.py
@@ -3,11 +3,15 @@ from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
+from ament_index_python.packages import get_package_share_directory
+
+import os
+
 def generate_launch_description():
     joy_launch = IncludeLaunchDescription(
       PythonLaunchDescriptionSource([os.path.join(
          get_package_share_directory('bringup'), 'launch'),
-         '/joy_launch.py'])
+         '/joy.launch.py'])
       )
     return LaunchDescription([
         joy_launch
