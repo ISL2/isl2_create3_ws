34eb76188d3956bc61ee2513ed17dcbed8def9e2
ISL2 <isl2.chula@gmail.com>
2023-07-05 01:43:41 +0000

    refactor

--- src/bringup/launch/joy.launch.py
+++ src/bringup/launch/joy.launch.py
@@ -9,21 +9,24 @@ import os
 
 
 def generate_launch_description():
+    joy_input_node = Node(
+        package='joy',
+        executable='joy_node',
+        name='joy_input'
+    )
+
+    joy_twist_node = Node(
+        package='teleop_twist_joy',
+        executable='teleop_node',
+        name='joy_twist',
+        parameters=[{
+            'axis_linear.x': 1,
+            'axis_angular.yaw': 3,
+            'enable_button': 5,
+        }]
+    )
 
     return LaunchDescription([
-        Node(
-            package='joy',
-            executable='joy_node',
-            name='joy_input'
-        ),
-        Node(
-            package='teleop_twist_joy',
-            executable='teleop_node',
-            name='joy_twist',
-            parameters=[{
-                'axis_linear.x': 1,
-                'axis_angular.yaw': 3,
-                'enable_button': 5,
-            }]
-        ),
+        joy_input_node,
+        joy_twist_node,
     ])
